28b8daf71f1a80d36dcf7a7836e0c0409b92b677
0PePo0 <155554095+0PePo0@users.noreply.github.com>
2025-05-15 00:57:21 +0300

    Add modal body

--- components/ConfirmationModal.vue
+++ components/ConfirmationModal.vue
@@ -7,6 +7,47 @@
     <UButton label="Зберегти маршрут" color="neutral" variant="subtle" />
 
     <template #body>
+      <div v-if="currentState === 'confirm'" class="sm:flex sm:items-start">
+        <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+          <UIcon name="i-heroicons-exclamation-triangle" class="h-6 w-6 text-blue-600" />
+        </div>
+        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+          <h3 class="text-lg leading-6 font-medium ">
+            Ви впевнені, що хочете зберегти цей маршрут?
+          </h3>
+          <div class="mt-2">
+            <p class="text-sm ">
+              Після цього ви зможете знайти його у списку своїх маршрутів!
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div v-else-if="currentState === 'loading'" class="text-center py-4">
+        <UIcon name="i-heroicons-arrow-path" class="animate-spin h-12 w-12  mx-auto" />
+        <p class="mt-4 text-lg ">Завантаження...</p>
+      </div>
+
+
+      <div v-else-if="currentState === 'success'" class="text-center py-4">
+        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+          <UIcon name="i-heroicons-check" class="h-6 w-6 text-green-600" />
+        </div>
+        <h3 class="mt-4 text-lg font-medium ">Маршрут успішно збережено!</h3>
+      </div>
+
+
+      <div v-else-if="currentState === 'error'" class="text-center py-4">
+        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+          <UIcon name="i-heroicons-x-mark" class="h-6 w-6 text-red-600" />
+        </div>
+        <h3 class="mt-4 text-lg font-medium ">Помилка!</h3>
+        <p class="mt-1 text-sm ">
+          Будь ласка, спробуйте пізніше.
+        </p>
+        <p v-if="errorMessage" class="mt-2 text-sm text-red-500">{{ errorMessage }}</p>
+      </div>
     </template>
 
     <template #footer>
