b4c9551d5770c59c474eed230688203de1e78c41
merely <128153965+meerreelly@users.noreply.github.com>
2025-05-15 00:44:38 +0300

    initial

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+﻿// app.config.ts
+export default defineAppConfig({
+    ui: {
+        modal: {
+            slots: {
+                footer:'justify-end border-none',
+                header:'border-none',
+                body:'border-none',
+            },
+        }
+    }
+})
